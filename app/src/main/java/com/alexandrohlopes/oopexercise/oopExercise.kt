f88a1345ffcb4db76d71339ba86589566fca0ef7
Alexandro H. Lopes <karkekoisaaeh@outlook.com>
2022-09-30 10:07:17 -0300

    Added method 'chargeBattery'

--- app/src/main/java/com/alexandrohlopes/oopexercise/oopExercise.kt
+++ app/src/main/java/com/alexandrohlopes/oopexercise/oopExercise.kt
@@ -1,13 +1,18 @@
 package com.alexandrohlopes.oopexercise
 
 fun main() {
-   var iphone99 = MobilePhone("iOS", "Apple", "iPhone 99 Pro")
-   var redmi4 = MobilePhone("Android", "Xiaomi", "Redmi Note 4")
+
    var pocoM3Pro = MobilePhone("Android", "Xiaomi", "Poco M3 Pro")
+   pocoM3Pro.chargeBattery(30) // The parameter is how much we will charge the battery
 }
 
-class MobilePhone(osName : String, brand : String, model : String) {
+class MobilePhone(osName : String, brand : String, model : String, battery : Byte = 0) {
     init {
-        println("Brand: $brand | Model: $model | OS: $osName")
+        println("Brand: $brand | Model: $model | OS: $osName | Battery: $battery")
+    }
+
+    fun chargeBattery(chargedBy: Byte) {
+        var battery = 0
+        println("The battery was $battery and it's ${battery + chargedBy} now.")
     }
 }
